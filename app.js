2ae415de7b24c979c704660d94b690574e5af66e
sofi614 <alvarengasofiajaqueline@gmail.com>
2026-07-03 11:33:11 -0300

    se creo el servidor express

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-import express from `express`;
+import express from "express";
+import sequelize from "./src/config/database.js";
+import movieRoutes from "./src/routes/movie.routes.js";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Api de peliculas funcionando");
+app.get("/", (req, res) => {
+  res.send("API REST de películas funcionando");
 });
 
-app.listen(3000,()=>{
-    console.log("Servidor corriendo en http://localhost:3000");
+app.use("/api", movieRoutes);
+
+try {
+  await sequelize.authenticate();
+  console.log("Conexión a MySQL exitosa");
+
+  await sequelize.sync();
+  console.log("Modelos sincronizados");
+} catch (error) {
+  console.log("Error al conectar con la base de datos:", error);
+}
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en http://localhost:3000");
 });
